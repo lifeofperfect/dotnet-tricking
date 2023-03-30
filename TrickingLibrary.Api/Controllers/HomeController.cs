05ed5ef903a0f8076f4a51ecc2a766a9bfdb6e7f
perfect igbadumhe <perfect.igbadumhe@gmail.com>
2023-03-30 02:35:32 +0100

    first commit

---
+++ TrickingLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+            
+        }
+
+        [HttpGet] 
+        public string Index()
+        {
+            return "Hello world";
+        }
+    }
+}
